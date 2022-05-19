d60ef5e8db713cb468cb7ad59a23e64bd918bd18
Nicolas Bueno <nickibueno@gmail.com>
2022-05-19 15:30:24 -0300

    atts js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,50 @@
-const links = document.querySelectorAll(".header-menu li")
-const linkAtual = window.location.href
+//ativar links do menu
+const links = document.querySelectorAll(".header-menu a");
+const linkAtual = window.location.href;
 
-console.log(linkAtual)
-console.log(linkAtual)
+function clicou(link) {
+    const url = window.location.href;
+    const href = link.href;
+
+    if (url.includes(href)) {
+        link.classList.add("ativo");
+    }
+
+
+}
+links.forEach(clicou)
+
+
+//ativar itens do orçamento
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+    const elemento = document.getElementById(parametro);
+    if (elemento) elemento.checked = true;
+
+
+}
+parametros.forEach(ativarProduto);
+
+
+//perguntas frequentes
+
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event) {
+    const pergunta = event.currentTarget;
+    const controls = pergunta.getAttribute("aria-controls");
+    const resposta = document.getElementById(controls);
+
+    resposta.classList.toggle("ativa");
+    const ativa = resposta.classList.contains("ativa");
+    pergunta.setAttribute("aria-expanded", ativa);
+
+}
+
+function eventosPerguntas(pergunta) {
+    pergunta.addEventListener("click", ativarPergunta);
+
+}
+perguntas.forEach(eventosPerguntas);
