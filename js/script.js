c3c4b44b3fff7f6cd7ff419be1943baea6aeb780
Nicolas Bueno <nickibueno@gmail.com>
2022-05-20 15:07:04 -0300

    finalizado

--- js/script.js
+++ js/script.js
@@ -48,3 +48,28 @@ function eventosPerguntas(pergunta) {
 
 }
 perguntas.forEach(eventosPerguntas);
+
+
+
+//galeria
+
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
+
+function trocarImagem(event) {
+    const img = event.currentTarget;
+    const media = matchMedia("(min-width: 1000px)").matches;
+    console.log(media)
+    if (media) {
+        galeriaContainer.prepend(img);
+    }
+
+
+}
+
+function eventosGaleria(img) {
+    img.addEventListener("click", trocarImagem);
+
+}
+
+galeria.forEach(eventosGaleria)
